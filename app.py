5b1114de0f514efdcaa64de26434ccf555be9457
agneevraha <70326746+agneevraha@users.noreply.github.com>
2023-05-14 21:55:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ with st.expander('Analyze CSV'):
          plt.show()
          st.pyplot()
          
-         if  df4['Analysis'] <= 0.5 and df4['Analysis'] >= -0.5 and df4['SubjAnalysis'] == 0.5:
+         if  df4['Analysis'] == 0 and df4['SubjAnalysis'] == 0.5:
             st.write(df4['Product_Description'])
          
         
